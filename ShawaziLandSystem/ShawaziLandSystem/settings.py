9a6e55e388772fa05dd96c474a1cbdc10d16582b
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-09-07 18:56:40 +0300

    added .env file

--- ShawaziLandSystem/ShawaziLandSystem/settings.py
+++ ShawaziLandSystem/ShawaziLandSystem/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,14 +78,17 @@ WSGI_APPLICATION = 'ShawaziLandSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+load_dotenv()
+
 DATABASES = {
      'default': {
        'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'shawazidatabase',
-        'USER': 'shawaziadmin',
-        'PASSWORD': 'shawazipassword',
-        'HOST': 'localhost',
-        'PORT': '5432'
+         'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST':os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
