59df02ee53d51ff72980d59163b9a16cc25b6104
kimura-khamali <kimurakhamali@gmail.com>
2024-09-25 14:23:13 +0300

    updated views.py and settings.py

--- api/views.py
+++ api/views.py
@@ -45,9 +45,9 @@ from .serializers import (
 )
 from django.shortcuts import render
 
-GOOGLE_VISION_CREDENTIALS = settings.GOOGLE_VISION_CREDENTIALS
-credentials =  vision.ImageAnnotatorClient(credentials=GOOGLE_VISION_CREDENTIALS)
-
+# GOOGLE_VISION_CREDENTIALS = settings.GOOGLE_VISION_CREDENTIALS
+# credentials =  vision.ImageAnnotatorClient(credentials=GOOGLE_VISION_CREDENTIALS)
+client = vision.ImageAnnotatorClient(credentials=settings.GOOGLE_VISION_CREDENTIALS)
 
 
 User = get_user_model()
