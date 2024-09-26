b169eefeb7a5e858409e0dd5e6c7904cad4faa46
kimura-khamali <kimurakhamali@gmail.com>
2024-09-26 08:28:01 +0300

    update asgi file

--- ShawaziLandSystem/asgi.py
+++ ShawaziLandSystem/asgi.py
@@ -18,7 +18,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from chatroom.consumers import ChatConsumer
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShawaziLandSystem.settings')
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
